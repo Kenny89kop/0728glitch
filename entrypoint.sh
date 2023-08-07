8c2924e68aec0817099716d3b7e1092d6358150b
Kenny89kop <137861378+Kenny89kop@users.noreply.github.com>
2023-08-07 16:50:55 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # 设置各变量，WS 路径前缀。(注意:伪装路径不需要 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
 WSPATH=${WSPATH:-'argo'}
-UUID=${UUID:-'71df24a4-1b04-46cd-9f60-b55917e299c9'}
+UUID=${UUID:-'e1820c3e-a5f5-49ec-b324-b698f0a287fc'}
 
 # 安装系统依赖
 check_dependencies() {
